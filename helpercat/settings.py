673bca4ea70d99de6cbc98833431a7020e8bcc26
Bridget Bangert <104116371+bridgetbangert@users.noreply.github.com>
2025-03-07 16:40:40 -0600

    Update database setting

--- helpercat/settings.py
+++ helpercat/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = "helpercat.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'poptartai',
+        'USER': 'admin',
+        'PASSWORD': 'hiIlikepie77!',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
+
 
 
 # Password validation
