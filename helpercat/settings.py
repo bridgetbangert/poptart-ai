39ffec6cc81582abb8f9d9aa8cc7dc537eb232ef
Bridget Bangert <bridgetmbangert@gmail.com>
2025-03-08 21:28:22 -0600

    Parameterize env variables

--- helpercat/settings.py
+++ helpercat/settings.py
@@ -19,19 +19,20 @@ db_pw = os.getenv("DB_PWD")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4ks+k+unapyjuo8i%)dv_&l&9^2$u($x2dln0qy7rz)#ub@#(7"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.128.127.253', 'localhost', 'poptartai.bridgetbangert.com']
+ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ['https://poptart-ai.bridgetbangert.com', 'https:/www.poptart-ai.bridgetbangert.com']
 
 # Application definition
 
@@ -81,12 +82,12 @@ WSGI_APPLICATION = "helpercat.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'poptartai',
-        'USER': 'admin',
-        'PASSWORD': db_pw,
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER_NM'),
+        "PASSWORD": os.getenv('DB_USER_PW'),
+        "HOST": os.getenv('DB_IP'),
+        "PORT": os.getenv('DB_PORT'),
     }
 }
 
